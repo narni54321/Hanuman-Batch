3180ec0b1767c418ec1dfc28de1473aa1171e5ec
narni54321 <122954876+narni54321@users.noreply.github.com>
2024-03-20 08:37:20 +0530

    Update Simple - Copy (5).java
    modified the code

--- Simple - Copy (5).java
+++ Simple - Copy (5).java
@@ -1,4 +1,6 @@
-//This Program to display "Hello Java" Message only
+//Purpose: This Program to display "Hello Java" Message only
+//Authodr: Narni Prasad
+//20th March 2024
 
 class Simple{  
     public static void main(String args[]){  
